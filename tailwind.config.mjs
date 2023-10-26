8a1b891efc068a7daedcbde9ceecc07db18e76d7
ianEddie <ianeddie2@gmail.com>
2023-10-26 19:48:30 -0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			primary: ['Raleway', 'sans-serif'],
+			secondary: ['Bungee', 'sans-serif']
+		},
+		extend: {},
+	},
+	plugins: [],
+}
